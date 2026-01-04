c2919e384810a41f0ff571956385527b357e6d38
kuldeepdevx <kuldeep.girase.work@gmail.com>
2026-01-05 00:13:55 +0530

    Add BankAccount example demonstrating proper encapsulation

--- src/OOPS/Encapsulation/Challenges/BANK/BANKACCOUNT/BankAccount.java
+++ src/OOPS/Encapsulation/Challenges/BANK/BANKACCOUNT/BankAccount.java
@@ -1,70 +1,98 @@
 package OOPS.Encapsulation.Challenges.BANK.BANKACCOUNT;
 
-public class BankAccount {
+public class BankAccount{
+    public static int totalAccounts;
 
-    private String accountHolderName;
     private long accountNumber;
-    private double Balance;
+    private String holderName;
+    private double accountBalance;
 
-    static {
-        System.out.println("*********************WELCOME TO OUR BANK*********************\n");
+    static{
+        System.out.println("******************************Welcome to bank******************************\n");
+        totalAccounts=0;
     }
 
-    public BankAccount(String accountHolderName,
-                       long accountNumber){
-        this.accountHolderName=accountHolderName;
-        this.accountNumber=accountNumber;
-            System.out.println("Thanks for opening bank account\n");
+    {
+        totalAccounts++;
+        accountBalance=0;
+    }
+
+    public BankAccount(int accountNumber,
+                       String holderName,double accountBalance){
+        if(accountBalance<500){
+            System.out.println("For opening account balance should be more than 500");
+        }
+        else{
+            this.accountBalance=accountBalance;
+            this.holderName=holderName;
+            this.accountNumber=accountNumber;
+            System.out.println("\n\nThanks for opening bank account "+this.holderName+"\n");
+        }
+    }
+
+    public String getHolderName(){
+        return holderName;
+    }
+
+    public void setHolderName(String name){
+        this.holderName=name;
+    }
+
+    public long getAccountNumber(){
+        return accountNumber;
+    }
+
+    public double getAccountBalance(){
+        return accountBalance;
+    }
+
+    public void deposite(double balance){
+        if(balance<=0){
+            System.out.println("Invalid Balance to deposite");
+        }else {
+            this.accountBalance += balance;
+            System.out.println("Deposite of rs:" + balance + " is succesfull");
+        }
+    }
+
+    public void withdraw(double balance){
+        if(balance>this.accountBalance){
+            System.out.println("Insufficient money to withdraw");
+        }else if(balance<=0) {
+            System.out.println("Invalid money to withdraw");
+        }else{
+            this.accountBalance -= balance;
+            System.out.println("Withdraw of rs:" + balance + " is succesfull");
+        }
+    }
+
+    public String checkBalance(){
+        return "Avl Bal is rs:"+this.accountBalance;
     }
 
     @Override
     public String toString() {
-        StringBuilder accountInfo=new StringBuilder();
-        accountInfo.append("Account Information[");
-        return accountInfo.append("Account no:").append(this.accountNumber)
-                .append(",Holder Name=").append(this.accountHolderName)
-                .append(",Balance=").append(this.Balance)
-                .append("]").toString();
+        return "Account Details=[" +
+                "Holder Name="+holderName+
+                ",Account Number="+accountNumber+
+                ",Account Balance="+accountBalance+
+                "]\n";
     }
-
-    public void setAccountHolderName(String accountHolderName){
-        this.accountHolderName=accountHolderName;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public void setAccountNumber(long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void depositMoney(double money){
-        if(money<=0){
-            System.out.println("Deposit should be positive");
-        }else {
-            this.Balance += money;
-            System.out.println("Deposit of rs-" + money + "/ is successful");
-        }
-    }
-
-    public void withdrawMoney(double money){
-        if(money<=0){
-            System.out.println("Withdraw should be in positive");
-        }else if(money>this.Balance){
-            System.out.println("Insufficient Balance!");
-        }else {
-            this.Balance -= money;
-            System.out.println("Withdraw of " + money + " is successful");
-        }
-    }
-
-    public void checkBalance(){
-        System.out.println("Your Account Balance is "+this.Balance);
-    }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/OOPS/Encapsulation/Challenges/BANK/BankCustomer.java
@@ -0,0 +1,35 @@
+package OOPS.Encapsulation.Challenges.BANK;
+import OOPS.Encapsulation.Challenges.BANK.BANKACCOUNT.BankAccount;
+
+public class BankCustomer{
+    public static void main(String[] args){
+
+        BankAccount customer1=new BankAccount(12345,
+                "Kuldeep", 500);
+
+        System.out.println(customer1.getHolderName());
+        System.out.println(customer1.getAccountNumber());
+        customer1.deposite(500);
+        customer1.withdraw(499.5);
+        System.out.println(customer1.getAccountBalance());
+        System.out.println(customer1.checkBalance());
+        System.out.println(customer1);
+
+
+        BankAccount customer2=new BankAccount(98765,
+                "Virat", 1000);
+
+        System.out.println(customer2.getHolderName());
+        System.out.println(customer2.getAccountNumber());
+        customer2.deposite(-500);
+        customer2.withdraw(10000);
+        System.out.println(customer2.getAccountBalance());
+        System.out.println(customer2.checkBalance());
+        System.out.println(customer2);
+
+        System.out.println(BankAccount.totalAccounts);
+    }
+}
+
+
+
