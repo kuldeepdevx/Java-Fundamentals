21c0e7090e8e65c468fdd506a2d0f3c15567c2f7
kuldeepdevx <kuldeep.girase.work@gmail.com>
2026-01-17 23:50:39 +0530

    Add example of exception

--- src/Exception_FileHandling/Exception.java
+++ src/Exception_FileHandling/Exception.java
@@ -1,4 +1,15 @@
 package Exception_FileHandling;
 
 public class Exception {
+    public static void main(String[] args){
+
+        try {
+            int x = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Error occurred");
+        } finally {
+            System.out.println("Always executed");
+        }
+
+    }
 }
