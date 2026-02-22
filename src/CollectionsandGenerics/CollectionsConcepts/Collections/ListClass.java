93ca1bd492258b9a6ddb7e8183e0726fd65871bf
kuldeepdevx <kuldeep.girase.work@gmail.com>
2026-02-22 22:24:46 +0530

    Implemented Queue collection example

--- src/CollectionsandGenerics/CollectionsConcepts/Collections/ListClass.java
+++ src/CollectionsandGenerics/CollectionsConcepts/Collections/ListClass.java
@@ -1,14 +1,18 @@
 package CollectionsandGenerics.CollectionsConcepts.Collections;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ListClass {
     public static void main(String[] args){
-
+        Scanner sc=new Scanner(System.in);
         List<Integer> list=new ArrayList<Integer>();
         list.add(10);
-        list.add(10);
-        System.out.println(list.toString());
+        list.add(12);
+        list.add(11);
+        list.add(13);
+        list.add(1,14);
+        System.out.println(list.indexOf(14));
+        list.set(0,1);
+        System.out.println(list);
         list.clear();
         System.out.println(list);
     }
---
+++ src/CollectionsandGenerics/CollectionsConcepts/Collections/QueueClass.java
@@ -0,0 +1,12 @@
+package CollectionsandGenerics.CollectionsConcepts.Collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueClass {
+    public static void main(String[] args){
+
+        Queue<Integer> queue=new LinkedList<>();
+
+    }
+}
